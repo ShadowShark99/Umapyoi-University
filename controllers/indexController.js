8dd5120cec867bbffc955572368be7946e3bb449
ShadowShark99 <kadenwchan@gmail.com>
2025-08-25 20:31:53 -0500

    Connect to umapyoi API

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -5,3 +5,25 @@ exports.getHome = async (req, res) => {
   res.render("index", {title: "Umapyoi University" ,umamusume: rows});
 }
 
+//example id: (1002, 4536), (1003, 4550), (1001m 4737)
+//gets called by view GET
+exports.expand = async (req, res) => {
+  console.log("expanding");
+  const {umaInfo} = req.body;
+  const get = `https://umapyoi.net/api/v1/character/1002`;
+  let umaName = "Special week";
+  const fetchUmas = async () => {
+
+      const response = await fetch(get);
+      console.log("response recieved");
+      console.log(response);
+      const result = await response.json();
+      console.log("result recieved");
+      console.log(result);
+      // setPokemonName(result.name);
+      // setSpriteUrl(result.sprites.front_default);
+  };
+
+  fetchUmas();
+  res.redirect("/");
+};
--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -3,5 +3,6 @@ const indexController = require("../controllers/indexController");
 const indexRouter = Router();
 
 indexRouter.get("/", indexController.getHome);
+indexRouter.post("/expand", indexController.expand);
 
 module.exports = indexRouter;
